2bad0d73289eabd85a41d32815c05b01fa1b3bd1
Russ Webber <russ.webber@greenroomrobotics.com>
2022-11-09 09:49:30 +1100

    chore: change tag to include ros release

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -1,10 +1,10 @@
 echo "Tagging version: $1"
-echo "ghcr.io/greenroom-robotics/ros_builder:$1"
-docker tag ros_builder:latest ghcr.io/greenroom-robotics/ros_builder:$1
-echo "ghcr.io/greenroom-robotics/ros_builder:latest"
-docker tag ros_builder:latest ghcr.io/greenroom-robotics/ros_builder:latest
+echo "ghcr.io/greenroom-robotics/ros_builder:galactic-$1"
+docker tag ros_builder:latest ghcr.io/greenroom-robotics/ros_builder:galactic-$1
+echo "ghcr.io/greenroom-robotics/ros_builder:galactic-latest"
+docker tag ros_builder:latest ghcr.io/greenroom-robotics/ros_builder:galactic-latest
 
-echo "ghcr.io/greenroom-robotics/ros_builder:cuda-$1"
-docker tag ros_builder:latest-cuda ghcr.io/greenroom-robotics/ros_builder:$1-cuda
-echo "ghcr.io/greenroom-robotics/ros_builder:cuda-latest"
-docker tag ros_builder:latest-cuda ghcr.io/greenroom-robotics/ros_builder:latest-cuda
+echo "ghcr.io/greenroom-robotics/ros_builder:galactic-cuda-$1"
+docker tag ros_builder:latest-cuda ghcr.io/greenroom-robotics/ros_builder:galactic-$1-cuda
+echo "ghcr.io/greenroom-robotics/ros_builder:galactic-cuda-latest"
+docker tag ros_builder:latest-cuda ghcr.io/greenroom-robotics/ros_builder:galactic-latest-cuda
