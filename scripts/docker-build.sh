49fe11e96f5b869b4a788f52632ad070ea36e9f1
Russ Webber <russ.webber@greenroomrobotics.com>
2022-11-08 17:17:09 +1100

    fix: cuda 11.7 is the version supported by stable pytorch

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 docker build --build-arg BASE_IMAGE="ubuntu:jammy" -f ./Dockerfile -t ros_builder:latest-jammy . 
-docker build --build-arg BASE_IMAGE="nvidia/cuda:11.8.0-devel-ubuntu22.04" -f ./Dockerfile -t ros_builder:latest-cuda-jammy .
+docker build --build-arg BASE_IMAGE="nvidia/cuda:11.7.0-devel-ubuntu22.04" -f ./Dockerfile -t ros_builder:latest-cuda-jammy .
