f4c2b4c74c1fc7d1c000217cfdc1422959c812df
Russ Webber <russ.webber@greenroomrobotics.com>
2022-11-09 09:57:08 +1100

    fix: change version tagging

--- scripts/docker-tag.sh
+++ scripts/docker-tag.sh
@@ -1,12 +1,12 @@
 echo "Tagging version: $1"
 echo "ROS version: $2"
 
-echo "ghcr.io/greenroom-robotics/ros_builder:$1"
-docker tag ros_builder:latest-$2 ghcr.io/greenroom-robotics/ros_builder:$1-$2
-echo "ghcr.io/greenroom-robotics/ros_builder:latest"
-docker tag ros_builder:latest-$2 ghcr.io/greenroom-robotics/ros_builder:latest-$2
+echo "ghcr.io/greenroom-robotics/ros_builder:$2-$1"
+docker tag ros_builder:latest-$2 ghcr.io/greenroom-robotics/ros_builder:$2-$1
+echo "ghcr.io/greenroom-robotics/ros_builder:$2-latest"
+docker tag ros_builder:latest-$2 ghcr.io/greenroom-robotics/ros_builder:$2-latest
 
-echo "ghcr.io/greenroom-robotics/ros_builder:cuda-$1"
-docker tag ros_builder:latest-$2-cuda ghcr.io/greenroom-robotics/ros_builder:$1-$2-cuda
-echo "ghcr.io/greenroom-robotics/ros_builder:cuda-latest"
-docker tag ros_builder:latest-$2-cuda ghcr.io/greenroom-robotics/ros_builder:latest-$2-cuda
+echo "ghcr.io/greenroom-robotics/ros_builder:$2-$1-cuda"
+docker tag ros_builder:latest-$2-cuda ghcr.io/greenroom-robotics/ros_builder:$2-$1-cuda
+echo "ghcr.io/greenroom-robotics/ros_builder:$2-latest-cuda"
+docker tag ros_builder:latest-$2-cuda ghcr.io/greenroom-robotics/ros_builder:$2-latest-cuda
