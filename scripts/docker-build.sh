5a197b269229ee87ce57ef32c3c38965ec864504
David Revay <daverevay@gmail.com>
2022-11-09 10:26:38 +1100

    fix: misc fixes

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,8 +3,7 @@ set -e
 DOCKER_BUILDKIT=1
 UBUNTU_VERSION="22.04"
 UBUNTU_CODENAME="jammy"
-ROS_CODENAME="humble"
 CUDA_VERSION="cuda:11.7.0-devel-ubuntu22.04"
 
-docker build --build-arg BASE_IMAGE="ubuntu:${UBUNTU_CODENAME}" -f ./Dockerfile -t ros_builder:latest-${ROS_CODENAME} . 
-docker build --build-arg BASE_IMAGE="nvidia/cuda:${CUDA_VERSION}" -f ./Dockerfile -t ros_builder:latest-cuda-${ROS_CODENAME} .
+docker build --build-arg BASE_IMAGE="ubuntu:${UBUNTU_CODENAME}" -f ./Dockerfile -t ros_builder:latest . 
+docker build --build-arg BASE_IMAGE="nvidia/cuda:${CUDA_VERSION}" -f ./Dockerfile -t ros_builder:latest-cuda .
