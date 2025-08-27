d37e66bc6844e0e3295682d46f6901f7f5faabec
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-08-27 14:24:04 +1000

    feat: build tensorrt images for jetson

--- scripts/docker-build.py
+++ scripts/docker-build.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/env python3
 
 import argparse
 import subprocess
@@ -7,7 +7,7 @@ from typing import Dict, List
 UBUNTU_VERSION = "24.04"
 UBUNTU_CODENAME = "noble"
 CUDA_VERSION = f"12.6.3-cudnn-devel-ubuntu{UBUNTU_VERSION}"
-TRT_CONTAINER_VERSION = "24.11"
+TRT_CONTAINER_VERSION = "25.08"
 
 ENV = Dict[str, str]
 
@@ -16,7 +16,6 @@ def build_image(
     base_image: str, ros_distro: str, arch: str, tags: List[str], push: bool = False, env: ENV = {}
 ):
     print(f"\033[92mBuilding image with base image {base_image} and tags {tags}\033[0m")
-
     command = [
         "docker buildx build",
         f"--platform linux/{arch}",
@@ -25,7 +24,6 @@ def build_image(
         "--provenance=false",
         " ".join([f"-t {tag}" for tag in tags]),
         "--push" if push else "",
-        "--network host" if use_host_networking else "",
         ".",
     ]
     command_str = " ".join(command)
@@ -34,7 +32,7 @@ def build_image(
         raise Exception(f"Failed to build image with command {command_str}")
 
 
-if __name__ == "__main__":
+def main():
     # Parse args
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -42,9 +40,7 @@ if __name__ == "__main__":
         required=True,
         help="ROS2 distro to build (e.g. galactic, humble, etc.)",
     )
-    parser.add_argument(
-        "--version", required=True, help="Version of the image (e.g. 1.0.0)"
-    )
+    parser.add_argument("--version", required=True, help="Version of the image (e.g. 1.0.0)")
     parser.add_argument(
         "--arch",
         required=True,
@@ -57,34 +53,31 @@ if __name__ == "__main__":
         help="skip building CUDA images",
     )
     parser.add_argument(
+        "--no-trt", required=False, action="store_true", help="skip building TensorRT images"
+    )
+    parser.add_argument(
         "--push",
         default=False,
         type=bool,
         help="Should we push the image to the registry?",
     )
-    parser.add_argument(
-        "--network-host",
-        default=False,
-        action="store_true",
-        help="Should we use host networking during the build process?",
-    )
     args = parser.parse_args()
 
     # Build images
 
-    if not args.no_cuda:
-        if args.arch == "amd64":
-            # 12.4 for x86 - This requires the cuda base to be built manually.
-            build_image(
-                base_image="ghcr.io/greenroom-robotics/cuda:12.4",
-                ros_distro=args.ros_distro,
-                arch=args.arch,
-                tags=[
-                    f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-cuda-12.4-{args.arch}",
-                    f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-cuda-12.4-{args.arch}",
-                ],
-                push=args.push,
-            )
+    # NOTE: not building an arm64 cuda image as it doesn't work on jetson, assumming arm64 == jetson
+    if not args.no_cuda and args.arch == "amd64":
+        # 12.4 for x86 - This requires the cuda base to be built manually.
+        build_image(
+            base_image="ghcr.io/greenroom-robotics/cuda:12.4",
+            ros_distro=args.ros_distro,
+            arch=args.arch,
+            tags=[
+                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-cuda-12.4-{args.arch}",
+                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-cuda-12.4-{args.arch}",
+            ],
+            push=args.push,
+        )
 
         # 12.6
         build_image(
@@ -96,7 +89,19 @@ if __name__ == "__main__":
                 f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-cuda-12.6-{args.arch}",
             ],
             push=args.push,
-            use_host_networking=args.network_host,
+        )
+
+    # TODO: add a trt image for x86 machines
+    if not args.no_trt and args.arch == "arm64":
+        build_image(
+            base_image=f"nvcr.io/nvidia/tensorrt:{TRT_CONTAINER_VERSION}-py3-igpu",
+            ros_distro=args.ros_distro,
+            arch=args.arch,
+            tags=[
+                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-tensorrt-{args.arch}",
+                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-tensorrt-{args.arch}",
+            ],
+            push=args.push,
         )
 
     build_image(
@@ -108,5 +113,8 @@ if __name__ == "__main__":
             f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-{args.arch}",
         ],
         push=args.push,
-        use_host_networking=True,
     )
+
+
+if __name__ == "__main__":
+    main()
