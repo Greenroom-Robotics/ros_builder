ca44e8e8599bf769e400bfe50d31977072f17395
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-09-02 14:38:39 +1000

    feat: combine cuda and trt images

--- scripts/docker-build.py
+++ scripts/docker-build.py
@@ -7,7 +7,11 @@ from typing import Dict, List
 UBUNTU_VERSION = "24.04"
 UBUNTU_CODENAME = "noble"
 CUDA_VERSION = f"12.6.3-cudnn-devel-ubuntu{UBUNTU_VERSION}"
-TRT_CONTAINER_VERSION = "25.08"
+# TODO: upgrade amd64 to also use 25.08
+TRT_CONTAINER_VERSIONS = {
+    "arm64": "25.08",
+    "amd64": "24.11",
+}
 
 ENV = Dict[str, str]
 
@@ -33,7 +37,7 @@ def build_image(
         raise Exception(f"Failed to build image with command {command_str}")
 
 
-if __name__ == "__main__":
+def main():
     # Parse args
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -47,17 +51,43 @@ if __name__ == "__main__":
         "--no-cuda", required=False, action="store_true", help="skip building CUDA images"
     )
     parser.add_argument(
-        "--no-trt", required=False, action="store_true", help="skip building TensorRT images"
-    )
-    parser.add_argument(
         "--push", default=False, type=bool, help="Should we push the image to the registry?"
     )
     args = parser.parse_args()
 
     # Build images
 
-    # NOTE: not building an arm64 cuda image as it doesn't work on jetson, assumming arm64 == jetson
-    if not args.no_cuda and args.arch == "amd64":
+    build_image(
+        base_image=f"ubuntu:{UBUNTU_CODENAME}",
+        ros_distro=args.ros_distro,
+        arch=args.arch,
+        tags=[
+            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-{args.arch}",
+            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-{args.arch}",
+        ],
+        push=args.push,
+    )
+
+    if args.no_cuda:
+        return
+
+    base_cuda_img = f"nvcr.io/nvidia/tensorrt:{TRT_CONTAINER_VERSIONS[args.arch]}-py3"
+    if args.arch == "arm64":
+        # jetson has an integrated gpu
+        base_cuda_img += "-igpu"
+
+    build_image(
+        base_image=base_cuda_img,
+        ros_distro=args.ros_distro,
+        arch=args.arch,
+        tags=[
+            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-tensorrt-{args.arch}",
+            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-tensorrt-{args.arch}",
+        ],
+        push=args.push,
+    )
+
+    if args.arch == "amd64":
         # 12.4 for x86 - This requires the cuda base to be built manually.
         build_image(
             base_image="ghcr.io/greenroom-robotics/cuda:12.4",
@@ -70,38 +100,6 @@ if __name__ == "__main__":
             push=args.push,
         )
 
-        # 12.6
-        build_image(
-            base_image=f"nvidia/cuda:{CUDA_VERSION}",
-            ros_distro=args.ros_distro,
-            arch=args.arch,
-            tags=[
-                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-cuda-12.6-{args.arch}",
-                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-cuda-12.6-{args.arch}",
-            ],
-            push=args.push,
-        )
 
-    # TODO: add a trt image for x86 machines
-    if not args.no_trt and args.arch == "arm64":
-        build_image(
-            base_image=f"nvcr.io/nvidia/tensorrt:{TRT_CONTAINER_VERSION}-py3-igpu",
-            ros_distro=args.ros_distro,
-            arch=args.arch,
-            tags=[
-                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-tensorrt-{args.arch}",
-                f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-tensorrt-{args.arch}",
-            ],
-            push=args.push,
-        )
-
-    build_image(
-        base_image=f"ubuntu:{UBUNTU_CODENAME}",
-        ros_distro=args.ros_distro,
-        arch=args.arch,
-        tags=[
-            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-{args.version}-{args.arch}",
-            f"ghcr.io/greenroom-robotics/ros_builder:{args.ros_distro}-latest-{args.arch}",
-        ],
-        push=args.push,
-    )
+if __name__ == "__main__":
+    main()
